2ce0b0ba2a3cc01018b2d9d3c14415ca1808c1f4
MBKY-766 <fengzhixin@drojian.dev>
2025-08-12 11:44:54 +0800

    UI基础控件：自定义控件布局的引用<include>，ListView，RecyclerView实现瀑布布局

---
+++ accountbook/src/main/java/com/example/accountbook/RecyclerViewActivity.kt
@@ -0,0 +1,15 @@
+package com.example.accountbook
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RecyclerViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view)
+
+    }
+}
---
+++ firstlineofcode/src/main/java/com/example/firstlineofcode/UICustomViewActivity.kt
@@ -0,0 +1,15 @@
+package com.example.firstlineofcode
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class UICustomViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_uicustom_view)
+
+    }
+}
