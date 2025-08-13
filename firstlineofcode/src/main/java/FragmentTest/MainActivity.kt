eff861383a0b6dc67c90a48c890f0456647a8d82
MBKY-766 <fengzhixin@drojian.dev>
2025-08-13 09:52:00 +0800

    Fragment动态添加，实现返回栈

--- firstlineofcode/src/main/java/FragmentTest/MainActivity.kt
+++ firstlineofcode/src/main/java/FragmentTest/MainActivity.kt
@@ -1,13 +1,32 @@
 package FragmentTest
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.firstlineofcode.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        button = findViewById(R.id.button)
+        button.setOnClickListener {
+            replaceFragment(AnotherRightFragment())
+        }
+        replaceFragment(RightFragment())
 
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.rightLayout, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
 }
