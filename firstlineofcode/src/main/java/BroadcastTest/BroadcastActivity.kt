3fee6762da0cd201298cbcbfbd68d380108936c6
MBKY-766 <fengzhixin@drojian.dev>
2025-08-13 14:53:59 +0800

    自定义广播

--- firstlineofcode/src/main/java/BroadcastTest/BroadcastActivity.kt
+++ firstlineofcode/src/main/java/BroadcastTest/BroadcastActivity.kt
@@ -1,35 +1,38 @@
 package BroadcastTest
 
-import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firstlineofcode.R
 
 class BroadcastActivity : AppCompatActivity() {
-    lateinit var timeChangeReceiver: TimeChangeReceiver
+//    lateinit var timeChangeReceiver: TimeChangeReceiver
+    lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_broadcast)
-        val intentFilter = IntentFilter()
+        button=findViewById(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent("com.example.broadcasttest.MY_BROADCAST")
+            intent.setPackage(packageName)
+            sendBroadcast(intent)
+        }
+        /*val intentFilter = IntentFilter()
         intentFilter.addAction("android.intent.action.TIME_TICK")
         timeChangeReceiver = TimeChangeReceiver()
         registerReceiver(timeChangeReceiver, intentFilter)
-
-
+*/
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        unregisterReceiver(timeChangeReceiver)
+//        unregisterReceiver(timeChangeReceiver)
     }
-    inner class TimeChangeReceiver: BroadcastReceiver() {
+    /*inner class TimeChangeReceiver: BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             Toast.makeText(context, "Time has changed", Toast.LENGTH_SHORT).show()
         }
 
-    }
+    }*/
 }
---
+++ firstlineofcode/src/main/java/BroadcastTest/receiver/MyBroadcastReceiver.kt
@@ -0,0 +1,12 @@
+package BroadcastTest.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyBroadcastReceiver: BroadcastReceiver()  {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Toast.makeText(context,"Received in MyBroadcastReceiver",Toast.LENGTH_SHORT).show()
+    }
+}
