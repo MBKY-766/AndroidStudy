4b1665434984d85a74222df611f200bcccba4e8e
MBKY-766 <fengzhixin@drojian.dev>
2025-08-14 11:39:18 +0800

    内联函数：inline 非局部返回 可以return 非内联函数：noinline 局部返回 不能使用return crossline：用于保证在内联函数的匿名内部类中一定不会使用return关键字，

--- firstlineofcode/src/main/java/AdvancedFunction/test.kt
+++ firstlineofcode/src/main/java/AdvancedFunction/test.kt
@@ -1,6 +1,7 @@
 package AdvancedFunction
 
 fun main() {
+
     val list = listOf("Apple", "Banana", "Orange", "Pear", "Grape")
     val result = StringBuilder().build {
         append("Start eating fruits.\n")
@@ -18,7 +19,7 @@ fun main() {
     println(result)*/
 }
 
-fun num1AndNum2(num1: Int, num2: Int, operation: (Int, Int) -> Int): Int {
+inline fun num1AndNum2(num1: Int, num2: Int, operation: (Int, Int) -> Int): Int {
     val result = operation(num1, num2)
     return result
 }
