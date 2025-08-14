9d968ee6c8ae2954843308d387a2bc2b605fb68a
MBKY-766 <fengzhixin@drojian.dev>
2025-08-14 11:07:06 +0800

    高阶函数：函数作为参数类型传递给函数

---
+++ firstlineofcode/src/main/java/AdvancedFunction/test.kt
@@ -0,0 +1,29 @@
+package AdvancedFunction
+
+fun main() {
+    val list = listOf("Apple", "Banana", "Orange", "Pear", "Grape")
+    val result = StringBuilder().build {
+        append("Start eating fruits.\n")
+        for (fruit in list) {
+            append(fruit).append("\n")
+        }
+        append("Ate all fruits.")
+    }
+    println(result.toString())
+    /*val num1 = 10
+    val num2 = 30
+    val result = num1AndNum2(num1, num2) { n1, n2 ->
+        n1 + n2
+    }
+    println(result)*/
+}
+
+fun num1AndNum2(num1: Int, num2: Int, operation: (Int, Int) -> Int): Int {
+    val result = operation(num1, num2)
+    return result
+}
+
+fun StringBuilder.build(block: StringBuilder.() -> Unit): StringBuilder {
+    block()
+    return this
+}
