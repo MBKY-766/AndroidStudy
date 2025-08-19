74f35bc8389d031527afdf10b36bdd2d56455e88
MBKY-766 <fengzhixin@drojian.dev>
2025-08-19 11:05:14 +0800

    前台通知栏显示Service服务

--- firstlineofcode/src/main/java/MyServiceTest/MyService.kt
+++ firstlineofcode/src/main/java/MyServiceTest/MyService.kt
@@ -1,10 +1,19 @@
 package MyServiceTest
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.firstlineofcode.R
 
 class MyService : Service() {
 
@@ -20,9 +29,30 @@ class MyService : Service() {
     override fun onBind(intent: Intent): IBinder {
         return mBinder
     }
+    @SuppressLint("ForegroundServiceType")
     override fun onCreate() {
         super.onCreate()
         Log.d("MyService", "onCreate executed")
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as
+                NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "my_service",
+                "前台Service通知",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            manager.createNotificationChannel(channel)
+        }
+        val intent = Intent(this, ServiceMainActivity::class.java)
+        val pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        val notification = NotificationCompat.Builder(this, "my_service")
+            .setContentTitle("This is content title")
+            .setContentText("This is content text")
+            .setSmallIcon(R.drawable.edit_bg)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.back_bg))
+            .setContentIntent(pi)
+            .build()
+        startForeground(1, notification)
     }
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
         Log.d("MyService", "onStartCommand executed")
